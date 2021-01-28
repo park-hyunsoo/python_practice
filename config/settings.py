ba1127835fde5519fd42a235498d7cd05f1fc6fd
hyun <qkrgustnq@naver.com>
2021-01-28 20:46:15 +0900

    model admin edit

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,10 @@ PROJECT_APPS = [
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'rooms.apps.RoomsConfig',
+    'reviews.apps.ReviewsConfig',
+    'reservations.apps.ReservationsConfig',
+    'lists.apps.ListsConfig',
+    'conversations.apps.ConversationsConfig',
 ]
 THIRD_PARTY_APPS = ['django_countries']
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
--- rooms/admin.py
+++ rooms/admin.py
@@ -4,12 +4,81 @@ from . import models
 # Register your models here.
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    """ Room Admin Definition """
 
-@admin.register(models.RoomType)
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    def count_amenities(self, obj):
+        # print(obj.amenities.all())
+        return obj.amenities.count()
+
+    count_amenities.short_description = "amenities"
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "host__gender",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country"
+    )
+    search_fields = ("=city","^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    fieldsets = (
+        (
+            "Basic Info",{"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",{"fields":("check_in","check_out","instant_book")},
+        ),
+        (
+            "Spaces",{"fields":("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "More About the Spaces",
+            {
+                "classes":("collapse",),
+                "fields":("amenities","facilities","house_rules")
+            },
+        ),
+        (
+            "Last Details",{"fields":("host", )},
+        )
+    )
+
+    ordering =('name', 'price')
+
+
+
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(models.Room)
-class RoomAdmin(admin.ModelAdmin):
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo Admin Definition """
     pass
